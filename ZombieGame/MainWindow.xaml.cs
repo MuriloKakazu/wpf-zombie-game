dabb9a5a766417ac0421d6f8ebcb2f5fce2a7067
Murilo Yuri Kakazu <yuri.kakazu@hotmail.com>
2017-11-01 04:42:23 -0200

    physics update

--- ZombieGame/MainWindow.xaml.cs
+++ ZombieGame/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ZombieGame.Physics;
 
 namespace ZombieGame
 {
@@ -23,6 +24,10 @@ namespace ZombieGame
         public MainWindow()
         {
             InitializeComponent();
+
+            float f = Mathf.Pow(2.006f, 8);
+
+
         }
     }
 }
