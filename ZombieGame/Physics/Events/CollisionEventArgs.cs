6beb6c404473819cb7ac504500fc505c9d6adfe9
081170039 <081170039@HPFS-CE-LAB6723>
2017-11-02 00:23:46 -0200

    physics update

---
+++ ZombieGame/Physics/Events/CollisionEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZombieGame.Game;
+
+namespace ZombieGame.Physics.Events
+{
+    public class CollisionEventArgs
+    {
+        public Entity Collider { get; protected set; }
+        public Vector CollisionDirection { get; protected set; }
+
+        public CollisionEventArgs(Entity collider, Vector collisionDirection)
+        {
+            Collider = collider;
+            CollisionDirection = collisionDirection;
+        }
+    }
+}
